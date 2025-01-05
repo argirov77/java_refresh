5a15d4a28c5f7192b541008ebcc1dc3cc6bf1921
argirov77 <164051115+argirov77@users.noreply.github.com>
2025-01-05 21:48:41 +0200

    3erd task

--- src/Task3.java
+++ src/Task3.java
@@ -1,14 +1,21 @@
+import java.util.HashSet;
+
 public class Task3 {
     public static void main(String [] args){
-
-
+        Student dima = new Student("Dmitriy Stoykov");
+        Student kathy = new Student("Kathy Morozova");
+        HashSet <Student> studentList= new HashSet<>();
+        studentList.add(dima);
+        studentList.add(kathy);
+        studentList.add(kathy);
+        System.out.println(studentList);
     }
 }
 class Student{
     private String name;
     private int id;
     private static int nextId=1;
-    public void Student(String n){
+    public Student(String n){
         this.name= n ;
         this.id = nextId++;
     }
